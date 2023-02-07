68f4617317f84246083d5df2a24a446e50a640d4
황승현 <ghkdsigm3@gmail.com>
2023-02-07 17:11:25 +0900

    first commit

---
+++ new-vue-ts/src/pages/Parent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <child parentMessage="안녕"></child>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import Child from '@/components/prop/Child.vue';
+
+@Component({
+    components: {
+        Child,
+    },
+})
+
+export default class Home extends Vue {}
+</script>
